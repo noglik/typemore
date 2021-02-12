7f5f2b7f21948f318a9cb59e5e5a4c15c684e4ec
noglik <yaroslaubuk@gmail.com>
2021-02-12 01:06:22 +0000

    Backspace and correct indication
    Still on termion :).
    Standart stdin allows to get cursor position.
    Coorect character has a green color, incorrect red.
    Space, enter and other special characters will brake everyting.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 extern crate termion;
 
-use std::io::{stdout, Write};
-use termion::async_stdin;
+use std::io::{stdin, stdout, Write};
+use termion::cursor::DetectCursorPos;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::{clear, color, cursor};
+use termion::{clear, color, cursor, event};
 
 static TEXT: &str = "Hello world!";
 
 const CORRECT: color::Green = color::Green;
+const DANGER: color::Red = color::Red;
+const DEFAULT: color::Reset = color::Reset;
 
 fn main() {
-    let mut stdin = async_stdin().keys();
+    let length = TEXT.chars().count();
+
+    let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
 
     write!(
@@ -26,19 +30,46 @@ fn main() {
     write!(stdout, "{pos}", pos = cursor::Goto(1, 1)).unwrap();
     stdout.flush().unwrap();
 
-    loop {
-        let input = stdin.next();
+    for c in stdin.events() {
+        let (x, _y) = stdout.cursor_pos().unwrap();
+        if usize::from(x) > length {
+            write!(
+                stdout,
+                "{pos}{color}Thanks!",
+                pos = cursor::Goto(1, 2),
+                color = color::Fg(DEFAULT)
+            )
+            .unwrap();
+            stdout.flush().unwrap();
+            break;
+        }
 
-        if let Some(Ok(key)) = input {
-            match key {
-                Key::Char(c) => {
+        let key = c.unwrap();
+
+        match key {
+            event::Event::Key(Key::Char(c)) => {
+                if c == TEXT.chars().nth((x - 1).into()).unwrap() {
                     write!(stdout, "{color}{}", c, color = color::Fg(CORRECT)).unwrap();
+                } else {
+                    write!(stdout, "{color}{}", c, color = color::Fg(DANGER)).unwrap();
+                }
+                stdout.flush().unwrap();
+            }
+            event::Event::Key(Key::Backspace) => {
+                if x != 1 {
+                    write!(
+                        stdout,
+                        "{pos}{color}{}{pos}",
+                        TEXT.chars().nth((x - 2).into()).unwrap(),
+                        pos = cursor::Goto(x - 1, 1),
+                        color = color::Fg(DEFAULT)
+                    )
+                    .unwrap();
                     stdout.flush().unwrap();
                 }
-                Key::Backspace => {}
-                Key::Ctrl('c') => break,
-                _ => {}
             }
+            event::Event::Key(Key::Ctrl('c')) => break,
+            _ => {}
         }
 
         stdout.flush().unwrap();
